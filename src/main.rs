332493f3a52170dbe43aae016502fa0d62cd03b8
jdh5772 <moonstne@naver.com>
2025-02-10 21:56:21 +0900

    변수, 함수명 수정 및 핸들러 함수 수정

--- src/main.rs
+++ src/main.rs
@@ -21,32 +21,6 @@ struct AppState {
     ip_map: Mutex<HashMap<String, bool>>,
 }
 
-#[tokio::main]
-async fn main() {
-    let user_set = Mutex::new(HashSet::new());
-    let (tx, _rx) = broadcast::channel(100);
-    let ip_map = Mutex::new(HashMap::new());
-
-    let app_state = Arc::new(AppState {
-        user_set,
-        tx,
-        ip_map,
-    });
-
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/websocket", get(websocket_handler))
-        .with_state(app_state);
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await
-    .unwrap();
-}
-
 async fn websocket_handler(
     ws: WebSocketUpgrade,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
@@ -59,13 +33,13 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>, addr: SocketAddr) {
     let addr = addr.to_string();
     let mut vec = addr.split(":");
     let ip = vec.next().unwrap();
-    let mut is_contained = false;
+    let mut is_already_contained = false;
 
     let (mut sender, mut receiver) = stream.split();
 
-    check_ip(&state, ip, &mut is_contained).await;
+    try_register_ip(&state, ip, &mut is_already_contained).await;
 
-    if is_contained {
+    if is_already_contained {
         let _ = sender
             .send(Message::Text(String::from("이미 접속중입니다!!")))
             .await;
@@ -75,23 +49,27 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>, addr: SocketAddr) {
     let mut username = String::new();
     while let Some(Ok(message)) = receiver.next().await {
         if let Message::Text(name) = message {
-            check_username(&state, &mut username, &name);
+            let name = name.trim();
+            let is_registered = try_register_username(&state, &mut username, name);
 
-            if username.trim().is_empty() {
-                remove_ip(&state, ip).await;
-                return;
-            }
-
-            if !username.is_empty() {
-                break;
-            } else {
+            if name.is_empty() {
                 let _ = sender
                     .send(Message::Text(String::from(
-                        "누군가 같은 닉네임 사용 중 !!! 새고로침 하세요.",
+                        "공백은 사용 불가 !!! 새로고침 하세요.",
                     )))
                     .await;
+                unregister_ip(&state, ip).await;
 
                 return;
+            } else if !is_registered {
+                let _ = sender
+                    .send(Message::Text(String::from(
+                        "누군가 같은 닉네임 사용 중 !!! 새로고침 하세요.",
+                    )))
+                    .await;
+                return;
+            } else {
+                break;
             }
         }
     }
@@ -121,11 +99,11 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>, addr: SocketAddr) {
     tokio::select! {
         _ = &mut send_task => {
             recv_task.abort();
-            remove_ip(&state, ip).await;
+            unregister_ip(&state, ip).await;
         },
         _ = &mut recv_task => {
             send_task.abort();
-            remove_ip(&state, ip).await;
+            unregister_ip(&state, ip).await;
         },
 
     };
@@ -137,30 +115,58 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>, addr: SocketAddr) {
     state.ip_map.lock().unwrap().remove(ip);
 }
 
-fn check_username(state: &AppState, string: &mut String, name: &str) {
+fn try_register_username(state: &AppState, string: &mut String, name: &str) -> bool {
     let mut user_set = state.user_set.lock().unwrap();
 
-    if !user_set.contains(name) {
-        user_set.insert(name.to_owned());
-
-        string.push_str(name);
+    if user_set.contains(name) {
+        return false;
     }
+
+    user_set.insert(name.to_owned());
+    string.push_str(name);
+    true
 }
 
 async fn index() -> Html<&'static str> {
     Html(std::include_str!("../chat.html"))
 }
 
-async fn check_ip(state: &AppState, ip: &str, is_contained: &mut bool) {
+async fn try_register_ip(state: &AppState, ip: &str, is_already_contained: &mut bool) {
     let mut ip_map = state.ip_map.lock().unwrap();
     if ip_map.contains_key(ip) {
-        *is_contained = true;
+        *is_already_contained = true;
     } else {
         ip_map.insert(ip.to_string(), true);
     }
 }
 
-async fn remove_ip(state: &AppState, ip: &str) {
+async fn unregister_ip(state: &AppState, ip: &str) {
     let mut ip_map = state.ip_map.lock().unwrap();
     ip_map.remove(ip);
+}
+
+#[tokio::main]
+async fn main() {
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(100);
+    let ip_map = Mutex::new(HashMap::new());
+
+    let app_state = Arc::new(AppState {
+        user_set,
+        tx,
+        ip_map,
+    });
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/websocket", get(websocket_handler))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
